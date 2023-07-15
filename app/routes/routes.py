d596845e80f811f64c0b61fb5f4c2780e49a1e76
Sundar <hariphjeshua777@gmail.com>
2023-07-15 12:55:01 +0530

    more swagger added

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, request, jsonify, make_response
 from app.db.database import DatabaseManager
 from app.configs import DDLandDMLConfigs
+from app.db.db_queries import *
+from flasgger import Swagger
+from app import app
+import pathlib
 from flask_jwt_extended import (
     jwt_required, create_access_token, get_jwt_identity
 )
-from app.db.db_queries import *
 
+
+current_file_path = pathlib.Path(__file__).parent.parent
 bp = Blueprint('routes', __name__)
 
 valid_username = 'john'
@@ -22,10 +27,31 @@ table_keys = {
     "teacher": teacher
 }
 delete_tbl_keys = {"delete_group": delete_group, "delete_subject": delete_subject}
+swagger = Swagger(app)
 
 
 @bp.route('/login', methods=['POST'])
 def login():
+    """
+    User Login
+    ---
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            username:
+              type: string
+            password:
+              type: string
+    responses:
+      200:
+        description: Access token generated successfully
+      401:
+        description: Invalid credentials
+    """
     username = request.json.get('username')
     password = request.json.get('password')
 
@@ -39,6 +65,17 @@ def login():
 @bp.route('/protected', methods=['GET'])
 @jwt_required()
 def protected():
+    """
+        Protected Route
+        ---
+        security:
+          - JWT: []
+        responses:
+          200:
+            description: Protected route accessed successfully
+          401:
+            description: Missing or invalid JWT token
+        """
     current_user = get_jwt_identity()
     return jsonify({'message': f'Protected route. User: {current_user}'}), 200
 
@@ -46,6 +83,17 @@ def protected():
 @bp.route('/', methods=['GET'])
 @jwt_required()
 def home_page():
+    """
+        Home Page
+        ---
+        security:
+          - JWT: []
+        responses:
+          200:
+            description: Tables Create Successfully
+          401:
+            description: Missing or invalid JWT token
+    """
     try:
         db_manger = DatabaseManager()
         db_manger.connect()
@@ -61,6 +109,17 @@ def home_page():
 @bp.route('/find_by_id/mark/<int:id_num>', methods=['GET'])
 @jwt_required()
 def find_mark(id_num):
+    """
+        Find Mark
+        ---
+        security:
+          - JWT: []
+        responses:
+          200:
+            description: {"message": {"number_of_students": 3}}
+          401:
+            description: Missing or invalid JWT token
+        """
     try:
         db_rec = get_mark(id_num)
         resp = {"message": db_rec[0]}
@@ -73,6 +132,18 @@ def find_mark(id_num):
 @bp.route('/find_by_id/mark_list/<int:student_id>', methods=['GET'])
 @jwt_required()
 def find_mark_list(student_id):
+    """
+        Find Mark List
+        ---
+        security:
+          - JWT: []
+        responses:
+          200:
+            description: {"message": {"mark": 90,"subject": "Science"}}
+          401:
+            description: Missing or invalid JWT token
+        """
+
     try:
         db_rec = get_mark_list(student_id)
         resp = {"message": db_rec[0]}
@@ -85,6 +156,17 @@ def find_mark_list(student_id):
 @bp.route('/find_by_id/students/<int:teacher_id>', methods=['GET'])
 @jwt_required()
 def find_student(teacher_id):
+    """
+        Find Mark List
+        ---
+        security:
+          - JWT: []
+        responses:
+          200:
+            description: {"message": {"number_of_students": 3}}
+          401:
+            description: Missing or invalid JWT token
+    """
     try:
         db_rec = get_students(teacher_id)
         resp = {"message": db_rec[0]}
@@ -97,6 +179,19 @@ def find_student(teacher_id):
 @bp.route('/find_all', methods=['GET'])
 @jwt_required()
 def find_all():
+    """
+        Find Mark List
+        ---
+        security:
+          - JWT: []
+        responses:
+          200:
+            description: {"message": [{"first_name": "John",
+    "group_name": 1,"last_name": "Doe","student_id": 1,"subject_title": "Group A" }, {"first_name": "Alice",
+    "group_name": 1,"last_name": "Johnson","student_id": 3,"subject_title": "Group A" }]}
+          401:
+            description: Missing or invalid JWT token
+    """
     try:
         db_rec = get_all()
         resp = {"message": db_rec}
@@ -109,6 +204,33 @@ def find_all():
 @bp.route('/add_details', methods=['POST'])
 @jwt_required()
 def add_details():
+    """
+    Find Mark List
+    ---
+    security:
+        - JWT: []
+
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            first_name:
+              type: string
+            last_name:
+              type: string
+            group_id:
+              type: integer
+            key:
+              type: string
+        responses:
+          200:
+            description: {"message": "Record added successfully"}
+          401:
+            description: Missing or invalid JWT token
+    """
     try:
         req_json = request.json
 
@@ -128,6 +250,30 @@ def add_details():
 @bp.route('/update_details/<int:id_num>', methods=['PUT'])
 @jwt_required()
 def update_details(id_num):
+    """
+        Find Mark List
+        ---
+    security:
+        - JWT: []
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            first_name:
+              type: string
+            last_name:
+              type: string
+            group_id:
+              type: integer
+        responses:
+          200:
+            description: {"message": "Record Updated successfully"}
+          401:
+            description: Missing or invalid JWT token
+    """
     try:
         req_json = request.json
 
@@ -146,6 +292,27 @@ def update_details(id_num):
 @bp.route('/delete_details/<int:id_num>', methods=['Delete'])
 @jwt_required()
 def delete_details(id_num):
+    """
+    Find Mark List
+    ---
+    security:
+        - JWT: []
+
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            key:
+              type: string
+        responses:
+          200:
+            description: {"message": "Record Deleted successfully"}
+          401:
+            description: Missing or invalid JWT token
+    """
     try:
         req_json = request.json
 
@@ -156,3 +323,4 @@ def delete_details(id_num):
         resp = {"message": "An Error occurred", "error": str(err)}
 
     return make_response(jsonify(resp))
+
